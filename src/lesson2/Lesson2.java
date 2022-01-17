1f0f3928a0d7535f394fb96fa25fbb5d2e0358a1
sborisov88 <sborisov88@gmail.com>
2022-01-17 20:52:21 +0300

    lesson_2

--- src/lesson2/Lesson2.java
+++ src/lesson2/Lesson2.java
@@ -28,4 +28,15 @@ package lesson2;
          }
      }
 
+     static boolean isLeapYear(int year) {
+         if (year % 4 != 0) {
+             return false;
+         } else if (year % 400 == 0) {
+             return true;
+         } else if (year % 100 == 0) {
+             return false;
+         } else {
+             return true;
+         }
+     }
  }
